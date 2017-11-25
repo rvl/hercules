dc730956ca7a1850d4c341aa6f40a6054daf375b
Rodney Lorrimar <dev@rodney.id.au>
2017-11-25 18:09:02 +0000

    Update nixpkgs version
    So we can get opaleye-0.6.0

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs/archive/57e768f58e7d2019ea19520226d5fb6e05e98c96.tar.gz)
+import (fetchTarball https://github.com/NixOS/nixpkgs/archive/9ce33dd8fb41ee9fcf0778a094772ebfbb532499.tar.gz)
