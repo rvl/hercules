3eb5fe4360bb5baa78e38eea57a6f568b9062018
Rodney Lorrimar <dev@rodney.id.au>
2017-11-27 08:10:30 +0000

    Un-break the config file syntax

--- backend/src/Data/Aeson/Extra.hs
+++ backend/src/Data/Aeson/Extra.hs
@@ -5,9 +5,18 @@ module Data.Aeson.Extra
 import Data.Aeson
 import qualified Data.Aeson.Types as JSON
 import Data.Aeson.Types           (camelTo2)
+import Data.Char
+
+modifierCamel :: String -> String
+modifierCamel = drop 1 . dropWhile (/= '_') . dropWhile (== '_') . camelTo2 '_'
 
 modifier :: String -> String
-modifier = drop 1 . dropWhile (/= '_') . dropWhile (== '_') . camelTo2 '_'
+modifier = firstLower . dropPrefix
+  where
+    dropPrefix :: String -> String
+    dropPrefix = dropWhile isLower
+    firstLower [] = []
+    firstLower (x:xs) = (toLower x:xs)
 
 prefixOptions :: JSON.Options
 prefixOptions = JSON.defaultOptions { JSON.fieldLabelModifier = modifier }
